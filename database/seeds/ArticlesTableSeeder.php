2f4dc907e1a7349b304f6c948b0f57fc19581f32
Luke McCann <developer-lukemccann@outlook.com>
2020-07-11 22:51:16 +0100

    Add Article seeder

---
+++ database/seeds/ArticlesTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Truncate existing records.
+        Article::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        // Create a few articles in db.
+        for($i = 0; $i < 50; $i++) {
+            Article::create([
+                'title' => $faker->sentence,
+                'body' => $faker->paragraph,
+            ]);
+        }
+    }
+}
