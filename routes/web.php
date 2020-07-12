6bfec1c20f0e980a384a9a2be61d4d716a16b16a
Luke McCann <developer-lukemccann@outlook.com>
2020-07-12 17:55:50 +0100

    Add basic routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Article;
+use Symfony\Component\HttpFoundation\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('articles', function() {
+    return Article::all();
+});
+
+Route::get('articles/{id}', function($id) {
+    return Article::find($id);
+});
+
+Route::post('articles', function(Request $request) {
+    return Article::create($request->all);
+});
+
+Route::put('articles/{id}', function(Request $request, $id) {
+    $article = Article::findOrFail($id);
+    $article->update($request->all);
+
+    return $article;
+});
+
+Route::delete('articles/{id}', function($id) {
+    Article::find($id)->delete();
+
+    return 204;
 });
